60649ac6a6594abfbf00d5b6f83ba43e93ba159a
Prathu <prathujoshi98@gmail.com>
2023-06-18 13:33:07 +0530

    Day5

---
+++ Day5/Day5.py
@@ -0,0 +1,18 @@
+# cousre: 100 days of code
+print("Hey I am a good boy\n and this is VS code")
+# this is a 'Single-Line comment'
+print('THis is a print statement')
+
+print('HEllo World!!!')  # PRinting hello world
+
+
+# ESCAPE SEQUENCE CHARACTER
+
+print("Hey I am a \"good boy\"\n and this is VS code")
+
+
+# MORE ABOUT PRINT STATEMENT
+
+print("HEy", 6, 7, sep="~")  # sep used to join any character
+print("HEy", 6, 7, sep="~", end="009\n")  # end is used to specify what to print at the end
+print("Prathu")
