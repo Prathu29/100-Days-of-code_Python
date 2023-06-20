a1ccb6a1acbaaf01cf52210db14570d6fedd068d
Prathu <prathujoshi98@gmail.com>
2023-06-20 15:51:11 +0530

    Day 6

---
+++ Day6/Day6.py
@@ -0,0 +1,26 @@
+#IN PYTHON EVERYTHING IS OBJECT
+
+
+
+a = 1
+b = True
+c = "Prathu"
+d = None
+e = complex(8,2)
+a1 = 9
+print(a)
+print(b)
+print(a1+a)
+
+print("The type of a is", type(a))  # type tells about the type
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of c is", type(e))
+
+
+list1 = [8,2.3,[-4,5],["apple","banana"]] #lsit is collection of data types
+print(list1)
+
+
+dict1 = {"name":"prathu","age":20,"canVote":True} # dictionary is a collection of key value pairs
+print(dict1)
