9f9362408a5bb771f311ab2efac2960c250801c8
Prathu <prathujoshi98@gmail.com>
2023-07-05 12:53:22 +0530

    Day11

---
+++ Day11/Day11.py
@@ -0,0 +1,22 @@
+name = "Prathu"
+friend = "Glitch"
+anotherFriend = 'oneshot'
+apple = '''He said,
+HI prathu
+I want tp eat an apple'''
+
+
+
+print("Hello, " + name)
+
+#print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6]) #throws an error
+print("Printing using a for loop\n")
+for character in name:
+    print(character)
