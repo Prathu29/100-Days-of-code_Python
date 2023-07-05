8273ba425d67d04a65129bb41dcbee20168fdb27
Prathu <prathujoshi98@gmail.com>
2023-07-05 12:52:49 +0530

    Day9

---
+++ Day9/Day9.py
@@ -0,0 +1,14 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+
+print(int(a) + int(b)) #type conversion from string to intiger
+print(a + b)#concatination of two strings
+
+
+#Implicite type casting
+#Converts higher data type to lower data type to prevent data loss
+c = 1.9
+d= 8
+print(c+d)
