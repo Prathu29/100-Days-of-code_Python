f7ffb21085c1e4fd2e39ef174cc0c854b77a013e
Prathu <prathujoshi98@gmail.com>
2023-07-05 12:51:28 +0530

    Day7

---
+++ Day7/day7.py
@@ -0,0 +1,10 @@
+# Python Calculator
+
+
+a = 50
+b = 5
+
+print("The value of", a, "+", b, "is: ", a+b)
+print("The value of", a, "-", b, "is: ", a-b)
+print("The value of", a, "*", b, "is: ", a*b)
+print("The value of", a, "/", b, "is: ", a/b)
