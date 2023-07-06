01a4323226874d099353f1fae51b5f4fbb424940
Prathu <prathujoshi98@gmail.com>
2023-07-06 21:55:36 +0530

    Day17

---
+++ Day17/Day17.py
@@ -0,0 +1,17 @@
+# name = 'Glitch'
+# for i in name:
+#     print(i)
+#     if(i == "b"):
+#         print("This is something special!")
+
+# colors = ["Red","Green","Yellow","Blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+for k in range(5):
+    print(k+1)
+
+for k in range(1,12,3):
+    print(k)
