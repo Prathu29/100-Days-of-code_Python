fcd1747a6e976b4c9825f7442bd8fc3072d2cbcb
Prathu <prathujoshi98@gmail.com>
2023-07-07 11:58:56 +0530

    Day19

---
+++ Day19/Day19.py
@@ -0,0 +1,14 @@
+# Break and continue
+
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 X", i+1,"=", 5*(i+1))
+# print("gettin out of the loop")
+
+
+for i in range(12):
+    if(i == 10):
+        print("Skip the iteration")
+        continue
+    print("5 X", i+1,"=", 5*(i+1))
