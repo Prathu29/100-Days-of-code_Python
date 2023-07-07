e317dbb22811f46ba607cc2eae8a5e8492fc5bbd
Prathu <prathujoshi98@gmail.com>
2023-07-07 13:03:36 +0530

    Day20

---
+++ Day20/Day20.py
@@ -0,0 +1,24 @@
+# Functions
+
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")
+
+def isLesser(a,b):
+    pass
+
+a= 9
+b = 8
+isGreater(a,b)
+calculateGmean(a,b)
+
+c = 7
+d  =8
+isGreater(c,d)
+calculateGmean(c,d)
