f9eefeef2e8122359e9f1544174ded873ee46de9
Prathu <prathujoshi98@gmail.com>
2023-07-13 21:21:01 +0530

    Day25

---
+++ Day25/Day25.py
@@ -0,0 +1,17 @@
+# OPERATIONS ON TUPLES
+# countries = ("India","France","USA")
+# countries1 = ("Argentina","Russia","Itly")
+# world = countries + countries1 # -> new tuple
+# print(world)
+
+
+# count method
+# tuple1 = (0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 3)
+# res = tuple1.count(3)
+# print(res)
+
+# INndex and len method 
+tuple1 = (0, 1, 1, 2, 2, 3, 31, 3, 4, 4, 4, 4, 3)
+# res = tuple1.index(3, 6, 8) # index (number,beggning no of slicing, end no of slicing)
+res = len(tuple1)
+print(res)
