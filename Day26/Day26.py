bad31d24431d71ff26774a074bf5b230b35b2d2b
Prathu <prathujoshi98@gmail.com>
2023-07-14 15:13:05 +0530

    Day26

---
+++ Day26/Day26.py
@@ -0,0 +1,14 @@
+# GOOD MORNING SIR SOLUTION
+
+import time
+t = time.strptime('%H:%M%S')
+hour = int(time.strftime('%H'))
+hour = int(input("Enter hour: "))
+print(hour)
+
+if(hour>=0 and hour<12):
+    print("Good morning sir")
+elif(hour>=12 and hour<17):
+    print("Good Aftrnoon Sir!")
+elif(hour>=17 and hour<0):
+    print("Good night sir")
